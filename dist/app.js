c9e5ed31d2a6d9cc7d91d73cacbfb42b68f76f21
Luki Klekner <lukaszklekner@gmail.com>
2023-07-22 13:06:58 +0200

    feat: display tasks

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,10 @@
-console.log("siemka! ts skonfigurowany! i dziala!!!");
+const tasks = ["Read a book", "Go for a walk", "Prepare meals"];
+const taskContainer = document.querySelector(".section__list--js");
+const render = () => {
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerHTML = task;
+        taskContainer.appendChild(taskElement);
+    });
+};
+render();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("siemka! ts skonfigurowany! i dziala!!!");
+const tasks: string[] = ["Read a book", "Go for a walk", "Prepare meals"];
+
+const taskContainer: HTMLElement = document.querySelector(".section__list--js");
+
+const render = () => {
+  tasks.forEach((task) => {
+    const taskElement: HTMLElement = document.createElement("li");
+    taskElement.innerHTML = task;
+    taskContainer.appendChild(taskElement);
+  });
+};
+
+render();
